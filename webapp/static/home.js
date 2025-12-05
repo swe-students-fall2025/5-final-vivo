a79495584c3a039e72842e6eb9e5db1748d80b21
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-12-05 11:32:49 -0500

    add leaflet osm nyc map to homepage

---
+++ webapp/static/home.js
@@ -0,0 +1,5 @@
+const map = L.map('map').setView([40.7128, -74.0060], 13); 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
