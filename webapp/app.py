13cfcecc2306f7b6c4a66900cc17d61739ea196a
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-12-05 10:31:16 -0500

    basic flask setup and additional files

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
-@app.get("/")
-def root():
-    return send_from_directory("static", "index.html")
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
