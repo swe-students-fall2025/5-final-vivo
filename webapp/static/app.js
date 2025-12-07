dd537b05198834fd2e9eae0788aa0c1f1849b517
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2025-12-07 16:26:06 -0500

    add lights and load 3d toilet model

--- webapp/static/app.js
+++ webapp/static/app.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 const camera = new THREE.PerspectiveCamera(
     10,
@@ -9,7 +10,33 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 13;
 
 const scene = new THREE.Scene();
+let bath;
+const loader = new GLTFLoader();
+loader.load('/static/plastic_toilet_cabin.glb',
+    function (gltf) {
+        bath = gltf.scene;
+        bath.scale.set(0.2, 0.2, 0.2); 
+        bath.position.set(0, -1, 0);  
+        scene.add(bath);
+        modelMove(); 
+    },
+    undefined,
+    function (error) {
+        console.error('Error loading model:', error);
+    }
+);
 
 const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container3D').appendChild(renderer.domElement);
+
+
+
+//light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
+scene.add(ambientLight);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+scene.add(topLight);
+
